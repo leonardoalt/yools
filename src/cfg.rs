cb2dadc77b1d39298edb76f360e460b22f2b4a25
Leo Alt <leo@ethereum.org>
2022-08-05 12:32:39 +0200

    init

---
+++ src/cfg.rs
@@ -0,0 +1,455 @@
+use yultsur::yul;
+
+use std::collections::HashMap;
+
+#[derive(Clone, Hash, Eq, PartialEq, Debug)]
+pub struct Node(usize);
+
+#[derive(Clone, Hash, PartialEq, Debug)]
+pub enum Edge {
+    Empty,
+    IfHeader,
+    IfTrueBranch(yul::Expression),
+    IfFalseBranch(yul::Expression),
+    AfterIf,
+    SwitchHeader,
+    SwitchCase(yul::Expression, Option<yul::Literal>),
+    AfterSwitch,
+    LoopHeader,
+    LoopBody(yul::Expression),
+    LoopFalse(yul::Expression),
+    LoopPost,
+    LoopBack,
+    AfterLoop,
+    Break,
+    Continue,
+    Leave,
+    FunctionEntry,
+    FunctionExit,
+}
+
+#[derive(Clone, Debug)]
+pub struct CFG {
+    pub graph: HashMap<Node, Vec<(Node, Edge)>>,
+    pub rev_graph: HashMap<Node, Vec<(Node, Edge)>>,
+    pub blocks: HashMap<Node, yul::Block>,
+    // (function_entry, function_exit)
+    pub functions: HashMap<String, (Node, Node)>,
+}
+
+impl CFG {
+    pub fn new() -> CFG {
+        CFG {
+            graph: HashMap::new(),
+            rev_graph: HashMap::new(),
+            blocks: HashMap::new(),
+            functions: HashMap::new(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CFGBuilder {
+    cfg: CFG,
+    counter: usize,
+    current: Node,
+    statement_acc: Vec<yul::Statement>,
+    // TODO replace \/ by a Stack
+    // (loop head, after loop), used by `continue` and `break`
+    loops: Vec<(Node, Node)>,
+    // TODO replace \/ by a Stack
+    // function name, used by `leave` to find `function_exit` from `cfg.functions`
+    fun_defs: Vec<String>,
+}
+
+impl CFGBuilder {
+    pub fn new() -> CFGBuilder {
+        CFGBuilder {
+            cfg: CFG::new(),
+            counter: 0,
+            current: Node(0),
+            statement_acc: vec![],
+            loops: vec![],
+            fun_defs: vec![],
+        }
+    }
+
+    pub fn build(ast: yul::Statement) -> CFG {
+        let mut builder = CFGBuilder::new();
+
+        builder.add_node(builder.current.clone());
+        builder.add_rev_node(builder.current.clone());
+
+        builder.visit_statement(&ast);
+
+        builder.save_acc();
+
+        builder.add_node(builder.current.clone());
+        builder.add_rev_node(builder.current.clone());
+
+        builder.cfg.clone()
+    }
+
+    fn new_node(&mut self) -> Node {
+        self.counter += 1;
+        Node(self.counter)
+    }
+
+    fn set_current(&mut self, node: Node) {
+        self.current = node;
+    }
+
+    fn save_acc(&mut self) {
+        self.cfg.blocks.insert(
+            self.current.clone(),
+            // TODO How can we force `self.statementes_acc` to move here?
+            yul::Block {
+                statements: self.statement_acc.clone(),
+            },
+        );
+        // TODO assert that the insertion happened
+        self.statement_acc.clear();
+    }
+
+    fn add_node(&mut self, from: Node) {
+        self.cfg.graph.entry(from).or_insert_with(|| vec![]);
+    }
+
+    fn add_rev_node(&mut self, to: Node) {
+        self.cfg.rev_graph.entry(to).or_insert_with(|| vec![]);
+    }
+
+    fn connect(&mut self, from: Node, to: Node, edge: Edge) {
+        self.add_node(from.clone());
+        self.add_rev_node(to.clone());
+
+        self.cfg.graph.get_mut(&from).unwrap().push((to.clone(), edge.clone()));
+        self.cfg.rev_graph.get_mut(&to).unwrap().push((from, edge));
+    }
+
+    fn visit_statement(&mut self, st: &yul::Statement) {
+        match st {
+            yul::Statement::Block(block) => {
+                self.visit_block(block);
+            }
+            yul::Statement::FunctionDefinition(..) => {}
+            yul::Statement::If(if_st) => {
+                self.visit_if(if_st);
+            }
+            yul::Statement::Switch(switch) => {
+                self.visit_switch(switch);
+            }
+            yul::Statement::ForLoop(for_loop) => {
+                self.visit_for(for_loop);
+            }
+            yul::Statement::Break => {
+                self.visit_break();
+            }
+            yul::Statement::Continue => {}
+            yul::Statement::Expression(yul::Expression::FunctionCall(fun_call))
+                if !Self::is_builtin(&fun_call.identifier.identifier) =>
+            {
+                self.visit_function_call(fun_call);
+            }
+            _ => self.statement_acc.push(st.clone()),
+        };
+    }
+
+    fn is_builtin(fun_name: &String) -> bool {
+        false
+    }
+
+    fn visit_block(&mut self, block: &yul::Block) {
+        block
+            .statements
+            .iter()
+            .for_each(|st| self.visit_statement(st));
+    }
+
+    fn create_function_def_nodes(&mut self, fun_name: &String) {
+        let fun_entry = self.new_node();
+        let fun_exit = self.new_node();
+        self.cfg
+            .functions
+            .insert(fun_name.clone(), (fun_entry.clone(), fun_exit.clone()));
+    }
+
+    fn function_def_nodes(&mut self, fun_name: &String) -> (Node, Node) {
+        if !self.cfg.functions.contains_key(fun_name) {
+            self.create_function_def_nodes(fun_name);
+        }
+
+        self.cfg.functions.get(fun_name).unwrap().clone()
+    }
+
+    fn create_function_def_cfg(&mut self, fun: &yul::FunctionDefinition) {
+        assert!(self.statement_acc.is_empty());
+
+        self.function_def_nodes(&fun.name.identifier);
+
+        let (entry, exit) = self
+            .cfg
+            .functions
+            .get_mut(&fun.name.identifier)
+            .unwrap()
+            .clone();
+
+        self.set_current(entry);
+        self.visit_block(&fun.block);
+        self.save_acc();
+
+        self.connect(self.current.clone(), exit, Edge::Empty);
+    }
+
+    fn visit_if(&mut self, ifs: &yul::If) {
+        self.save_acc();
+
+        let header = self.new_node();
+        let true_branch = self.new_node();
+        let after_if = self.new_node();
+
+        self.connect(self.current.clone(), header.clone(), Edge::IfHeader);
+        self.connect(
+            header.clone(),
+            true_branch.clone(),
+            Edge::IfTrueBranch(ifs.expression.clone()),
+        );
+        self.connect(
+            header,
+            after_if.clone(),
+            Edge::IfFalseBranch(ifs.expression.clone()),
+        );
+        self.connect(
+            true_branch.clone(),
+            after_if.clone(),
+            Edge::AfterIf,
+        );
+
+        self.set_current(true_branch);
+        self.visit_block(&ifs.block);
+        self.save_acc();
+
+        self.set_current(after_if);
+    }
+
+    fn visit_switch(&mut self, switch: &yul::Switch) {
+        self.save_acc();
+
+        let header = self.new_node();
+        let after = self.new_node();
+
+        self.connect(self.current.clone(), header.clone(), Edge::LoopHeader);
+        self.connect(header.clone(), after.clone(), Edge::AfterSwitch);
+
+        switch.cases.iter().for_each(|case| {
+            let case_node = self.new_node();
+            self.connect(
+                header.clone(),
+                case_node.clone(),
+                Edge::SwitchCase(switch.expression.clone(), case.literal.clone()),
+            );
+            self.connect(case_node.clone(), after.clone(), Edge::AfterSwitch);
+
+            self.set_current(case_node.clone());
+            self.visit_block(&case.block);
+            self.save_acc();
+        });
+
+        self.set_current(after);
+    }
+
+    fn visit_for(&mut self, for_loop: &yul::ForLoop) {
+        // TODO make this work \/
+        //self.statement_acc.append(&for_loop.pre.statements.clone());
+        self.save_acc();
+
+        let header = self.new_node();
+        let body = self.new_node();
+        let post = self.new_node();
+        let after = self.new_node();
+
+        self.connect(self.current.clone(), header.clone(), Edge::LoopHeader);
+        self.connect(
+            header.clone(),
+            body.clone(),
+            Edge::LoopBody(for_loop.condition.clone()),
+        );
+        self.connect(
+            header.clone(),
+            after.clone(),
+            Edge::LoopFalse(for_loop.condition.clone()),
+        );
+        self.connect(body.clone(), post.clone(), Edge::LoopPost);
+        self.connect(body.clone(), after.clone(), Edge::AfterLoop);
+        self.connect(post.clone(), header.clone(), Edge::LoopBack);
+
+        self.loops.push((header, after.clone()));
+
+        self.set_current(body);
+        self.visit_block(&for_loop.body);
+        self.save_acc();
+
+        self.set_current(post);
+        self.visit_block(&for_loop.post);
+        self.save_acc();
+
+        // TODO make this work
+        //self.loops.pop_back();
+        self.set_current(after);
+    }
+
+    fn visit_break(&mut self) {
+        self.save_acc();
+
+        self.connect(
+            self.current.clone(),
+            self.loops.last().unwrap().1.clone(),
+            Edge::Break,
+        );
+
+        let ghost = self.new_node();
+        self.set_current(ghost);
+        self.save_acc();
+    }
+
+    fn visit_continue(&mut self) {
+        self.save_acc();
+
+        self.connect(
+            self.current.clone(),
+            self.loops.last().unwrap().0.clone(),
+            Edge::Continue,
+        );
+
+        let ghost = self.new_node();
+        self.set_current(ghost);
+        self.save_acc();
+    }
+
+    fn visit_leave(&mut self) {
+        self.save_acc();
+
+        self.connect(
+            self.current.clone(),
+            self.cfg
+                .functions
+                .get(self.fun_defs.last().unwrap())
+                .unwrap()
+                .1
+                .clone(),
+            Edge::Leave,
+        );
+
+        let ghost = self.new_node();
+        self.set_current(ghost);
+        self.save_acc();
+    }
+
+    fn visit_function_call(&mut self, fun_call: &yul::FunctionCall) {
+        self.save_acc();
+
+        let after = self.new_node();
+
+        let (entry, exit) = self.function_def_nodes(&fun_call.identifier.identifier);
+
+        self.connect(self.current.clone(), entry.clone(), Edge::FunctionEntry);
+        self.connect(exit.clone(), after.clone(), Edge::FunctionExit);
+
+        self.set_current(after);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use yultsur::yul;
+    use yultsur::yul_parser;
+
+    #[test]
+    fn yul_block() {
+        let block = yul_parser::parse_block("{ let a := 2 }");
+
+        let cfg = CFGBuilder::build(yul::Statement::Block(block));
+
+        println!("{:?}", cfg);
+    }
+
+    #[test]
+    fn yul_if_basic() {
+        let block = yul_parser::parse_block("{ if 1 { } }");
+
+        let cfg = CFGBuilder::build(yul::Statement::Block(block));
+
+        println!("{:?}", cfg);
+    }
+
+    #[test]
+    fn yul_if() {
+        let block = yul_parser::parse_block("{ let a := 2 if a { a := 0 } let b := 666 }");
+
+        let cfg = CFGBuilder::build(yul::Statement::Block(block));
+
+        println!("{:?}", cfg);
+    }
+
+    // TODO `switch` is actually buggy in the parser
+    #[test]
+    fn yul_switch_basic() {
+        let block = yul_parser::parse_block("{ switch 3 case 0 { } default { } }");
+
+        let cfg = CFGBuilder::build(yul::Statement::Block(block));
+
+        println!("{:?}", cfg);
+    }
+
+    #[test]
+    fn yul_for_basic() {
+        let block = yul_parser::parse_block("{ for {} 1 {} {} }");
+
+        let cfg = CFGBuilder::build(yul::Statement::Block(block));
+
+        println!("{:?}", cfg);
+    }
+
+    #[test]
+    fn yul_for() {
+        let block = yul_parser::parse_block("{ let a := 2 for { a := 3 } a { a := 4 } { a := 5 } a := 6}");
+
+        let cfg = CFGBuilder::build(yul::Statement::Block(block));
+
+        println!("{:?}", cfg);
+    }
+
+    #[test]
+    fn yul_for_break_basic() {
+        let block = yul_parser::parse_block("{ for {} 1 { break } {} }");
+
+        let cfg = CFGBuilder::build(yul::Statement::Block(block));
+
+        println!("{:?}", cfg);
+    }
+
+    #[test]
+    fn yul_for_continue_basic() {
+        let block = yul_parser::parse_block("{ for {} 1 { continue } {} }");
+
+        let cfg = CFGBuilder::build(yul::Statement::Block(block));
+
+        println!("{:?}", cfg);
+    }
+
+    #[test]
+    fn if_statement() {
+        let if_st = yul::If {
+            expression: yul::Expression::Literal(yul::Literal {
+                literal: "cond".to_string(),
+                yultype: None,
+            }),
+            block: yul::Block { statements: vec![] },
+        };
+
+        let cfg = CFGBuilder::build(yul::Statement::If(if_st));
+
+        println!("{:?}", cfg);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+use yultsur::yul;
+use yultsur::yul_parser;
+
+use yools::symb_exec::*;
+
+fn main() {
+    let yul_file = env::args()
+        .nth(1)
+        .expect("Expected Yul .yul file");
+
+    let mut file = File::open(yul_file).unwrap();
+    let mut content = String::new();
+    file.read_to_string(&mut content).unwrap();
+
+    let ast = yul_parser::parse_block(&content);
+
+    SymbExecEngine::encode(yul::Statement::Block(ast));
+
+
+}
